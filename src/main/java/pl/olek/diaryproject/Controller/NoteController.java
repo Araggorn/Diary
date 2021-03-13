02e4d8f34e97c9fc05b79b14bfc7ba0485f90b80
Araggorn <zerelik@gmail.com>
2021-03-13 14:13:45 +0100

    7. Add note and get by id

--- src/main/java/pl/olek/diaryproject/Controller/NoteController.java
+++ src/main/java/pl/olek/diaryproject/Controller/NoteController.java
@@ -4,9 +4,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import pl.olek.diaryproject.Dto.NoteDto;
 import pl.olek.diaryproject.Service.NoteService;
 
+import java.net.URI;
 import java.util.List;
 
 @Slf4j
@@ -25,11 +27,27 @@ private final NoteService noteService;
         return noteService.getListOfAllNotes();
     }
 
-    @DeleteMapping("note/delete/{id}")
+    @DeleteMapping("/note/delete/{id}")
     public ResponseEntity<?> deleteNote(@PathVariable Long id) {
         log.info("Request to deleto note: {}", id);
         noteService.deleteById(id);
         return ResponseEntity.ok().build();
+    }
 
+    @GetMapping("/note/{id}")
+    public ResponseEntity<NoteDto> getNoteById(@PathVariable Long id){
+        return noteService.findById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+    @PostMapping("/note/add")
+    public ResponseEntity<NoteDto> noteAdd(NoteDto noteDto){
+        NoteDto savedNote = noteService.addNote(noteDto);
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedNote.getId())
+                .toUri();
+        return ResponseEntity.created(location).body(savedNote);
     }
 }
