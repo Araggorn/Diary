468732b188ae27d0b11698776e16d4af04cb6acf
Araggorn <zerelik@gmail.com>
2021-03-24 20:32:45 +0100

    xx

--- src/main/java/pl/olek/diaryproject/repository/NoteSnapshotRepo.java
+++ src/main/java/pl/olek/diaryproject/repository/NoteSnapshotRepo.java
@@ -4,8 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import pl.olek.diaryproject.entity.NoteSnapshot;
 
+import java.util.Set;
+
 @Repository
 public interface NoteSnapshotRepo extends JpaRepository<NoteSnapshot, Long> {
-
+ 
+    Set<NoteSnapshot> findAllByDeletedIsFalse = null;
 
 }
